ef1f0de5537dbb810fad9fbad20ffb710a8fc323
Tyler Pho <owner@Tyler-Phos-MacBook-Pro.local>
2020-02-05 16:06:45 -0700

    created test files for JS files

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+// Constant Variables and Base Arrays
 const inquirer = require("inquirer");
-const Manager = require("./")
+const Manager = require("./lib/Manager");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Engineer");
+
