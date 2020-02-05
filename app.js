8f826d6a2fff4e95de8f7a86c66fa3c9fe22b319
Tyler Pho <owner@Tyler-Phos-MacBook-Pro.local>
2020-02-05 16:20:40 -0700

    created inquirer prompt functions

--- app.js
+++ app.js
@@ -5,3 +5,60 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Engineer");
 
+
+
+// Inquirer prompts for user inputs
+function promptName() {
+    const name = inquirer.prompt({
+        type: "input",
+        message: "What is your Employee's name?",
+        name: "name"
+    });
+    return name;
+}
+
+function promptEmail() {
+    const email = inquirer.prompt({
+        type: "input",
+        message: "What is your Employee's company email?",
+        name: "email"
+    });
+    return email;
+}
+
+function promptRole(){
+    const role = inquirer.prompt({
+        type: "list",
+        message: "What is your Employee's position?",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern"]
+    });
+    return role;
+}
+
+function promptManager() {
+    const officeNumber = inquirer.prompt({
+        type: "input",
+        message: "What is your Manager's office number?",
+        name: "officeNumber",
+    });
+    return officeNumber;
+}
+
+function promptEngineer() {
+    const github = inquirer.prompt({
+        type: "input",
+        message: "What is the Engineer's GitHub username?",
+        name: "username"
+    });
+    return github;
+}
+
+function promptIntern() {
+    const school = inquirer.prompt ({
+        type: "input",
+        message: "Where does the Intern attend?",
+        name: "attend"
+    })
+    return school;
+}
