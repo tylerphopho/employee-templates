120eb54f3cfc5496403dca73658f78f5086eeb5f
Tyler Pho <owner@Tyler-Phos-MacBook-Pro.local>
2020-02-08 15:32:40 -0700

    created generateHTML function but does not work

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
 // Constant Variables and Base Arrays
+
+// NPM Packages
 const inquirer = require("inquirer");
+const fs = require("fs-extra");
+const util = require("util");
+const writeFile = util.promisify(fs.writeFile);
+
+// Routes for files
 const Manager = require("./lib/Manager");
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Engineer");
-
-
-// Run function 
-
-init();
-
-async function init() {
-    try {
-        let {name} = await promptName();
-        let {id} = await promptID();
-        let {email} = await promptEmail();
-        const {role} = await promptRole();
-        switch(role) {
-            case "Manager":
-                const {officeNumber} = await promptManager(name, id, email, role);
-                break;
-            case "Engineer":
-                const {github} = await promptEngineer(name, id, email, role);
-                break;
-            case "Intern":
-                const {school} = await promptIntern(name, id, email, role);
-                break;
-        }
-    }
-    catch(err){
-        console.log(err);
-    }
-}
-
+const Intern = require("./lib/Intern");
+const generateHTML = require("./templates/generateHTML");
+const generateCard = require("./templates/generateHTML");
+let employeeArray = [];
+let employeeCards = "";
 
 // Inquirer prompts for user inputs
 function promptName() {
@@ -98,3 +80,77 @@ function promptIntern() {
     })
     return school;
 }
+
+
+// Runs the APP
+async function init() {
+    try {
+        let {name} = await promptName();
+        let {id} = await promptID();
+        let {email} = await promptEmail();
+        const {role} = await promptRole();
+        switch(role) {
+            case "Manager":
+                let {officeNumber} = await promptManager();
+                let manager = new Manager(name, id, email, role, officeNumber)
+                employeeArray.push(manager)
+                console.log("New Manager added!");
+                break;
+            case "Engineer":
+                const {github} = await promptEngineer(name, id, email, role);
+                let engineer = new Engineer(name, id, email, role, github)
+                employeeArray.push(engineer)
+                console.log("New Engineer added!");
+                break;
+            case "Intern":
+                const {school} = await promptIntern(name, id, email, role);
+                let intern = new Intern(name, id, email, role, school)
+                employeeArray.push(intern)
+                console.log("New Intern added!");
+                break;
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+    done = await inquirer.prompt({
+        type: "list",
+        name: "add",
+        message: "Would you like to add another employee?",
+        choices: ["Yes", "No"]
+    });
+}while (done.finish === "Yes")
+console.log(employeeArray)
+
+let employeeCards = employeeArray.map((team) =>{
+    console.log(employee)
+    let role;
+    let htmlText;
+    if(employee.role === "Manager"){
+        role = employee.officeNumber;
+        htmlText = "Office Number: ";
+    }else if(employee.role === "Engineer"){
+        role = employee.github;
+        htmlText = "GitHub: ";
+    }else {
+        role = employee.school
+        htmlText = "School: ";
+    }    
+    return `<div class="card">
+    <div class="card-header bg-primary">
+      <h2 class="text-white">${employee.name}</h2>
+      <h3 class="text-white">${employee.role}</h3></div>
+    <div class="card-body">
+    <p class="card-text"><strong>ID: </strong>${employee.id}</p>
+      <p class="card-text"><strong>Email: </strong>${employee.email}</p>
+      <p class="card-text"><strong>${htmlText}</strong>${role}</p>
+    </div>
+  </div>`
+    }).join("");
+
+
+    //generates the HTML file in the output folder
+    const createHTML = generateHTML(teamCards);
+    writeFile("./output/Team.html",createHTML);
+
+init();
